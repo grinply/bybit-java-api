22c5ce6838d75713c6d7518e646760ca61ca35d0
Victor Ferreira Teixeira da Silva <victor.silva@grinply.com>
2025-05-05 09:05:08 -0300

    fix: temporarily restore lombok import

--- src/main/java/com/grinply/api/client/websocket/impl/WebsocketStreamClientImpl.java
+++ src/main/java/com/grinply/api/client/websocket/impl/WebsocketStreamClientImpl.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.Getter;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
